8a72fb45a2891bc9fd2ed8dce5b9ee92e857a693
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-04-02 20:42:14 -0600

    Add CategoriesSection to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CategoriesSection from "./(LandingPageComponents)/CategoriesList";
 import HeroSection from "./(LandingPageComponents)/Hero";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <CategoriesSection />
     </main>
   );
 }
