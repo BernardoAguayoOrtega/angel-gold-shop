da1042625f4daba98816f1e5bc3a4d3addbb5f1e
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-04-16 21:01:55 -0600

    Refactor CartSidebar component to improve styling and add overlay functionality

--- app/(GeneralComponents)/CartSidebar.tsx
+++ app/(GeneralComponents)/CartSidebar.tsx
@@ -1,36 +1,39 @@
 // components/Sidebar.tsx
 
-import React from 'react';
+import React from "react";
 
 interface SidebarProps {
   isOpen: boolean;
-  onClose?: () => void;
+  onClose: () => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   return (
-    <div
-      className={`fixed top-0 right-0 w-1/3 h-full bg-white shadow-md z-50 transform transition-transform ${
-        isOpen ? 'translate-x-0' : 'translate-x-full'
-      }`}
-    >
-      <button
-        onClick={onClose}
-        className="text-black p-2 focus:outline-none"
-      >
-        ×
-      </button>
-      <div className="p-4">
-        {/* Content of your sidebar */}
-        <div className="flex flex-col space-y-4">
-          {/* Map through your sidebar items here */}
-          <a href="#" className="text-gray-700 hover:text-gray-900">
-            Sample Item
-          </a>
-          {/* ... other items */}
+    <>
+      {isOpen && (
+        <div
+          className='fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-40'
+          onClick={onClose}></div>
+      )}
+      <div
+        className={`fixed top-0 right-0 w-full sm:w-1/3 h-full bg-white shadow-md z-50 transform transition-transform ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}>
+        <button onClick={onClose} className='text-black p-2 focus:outline-none'>
+          ×
+        </button>
+        <div className='p-4'>
+          {/* Content of your sidebar */}
+          <div className='flex flex-col space-y-4'>
+            {/* Map through your sidebar items here */}
+            <a href='#' className='text-gray-700 hover:text-gray-900'>
+              Sample Item
+            </a>
+            {/* ... other items */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
